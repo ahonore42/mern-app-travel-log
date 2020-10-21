33674798f825d6b9411edcdbfa255735d976a377
Andre Pato <anpato1994@gmail.com>
2020-10-21 03:04:37 -0400

    finish comment api

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -2,17 +2,46 @@ const { TravelLog, Comment } = require('../db/schema')
 
 const CreateComment = async (req, res) => {
   try {
-  } catch (error) {}
+    const comment = new Comment({ ...req.body, user_id: req.params.user_id })
+    await TravelLog.update(
+      { _id: req.params.post_id },
+      {
+        $push: {
+          comments: comment
+        }
+      }
+    )
+    comment.save()
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
 }
 
 const RemoveComment = async (req, res) => {
   try {
-  } catch (error) {}
+    await Comment.deleteOne({ _id: req.params.comment_id })
+    const updatedPost = await TravelLog.findByIdAndUpdate(
+      req.params.post_id,
+      { $pull: { comments: { _id: req.params.comment_id } } },
+      { upsert: true, new: true }
+    )
+    res.send(updatedPost)
+  } catch (error) {
+    throw error
+  }
 }
 
 const UpdateComment = async (req, res) => {
   try {
-  } catch (error) {}
+    const updatedtComment = await Comment.findByIdAndUpdate(
+      req.params.comment_id,
+      { ...req.body }
+    )
+    res.send(updatedtComment)
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,7 +1,7 @@
 const Router = require('express').Router()
 const CommentController = require('../controllers/CommentController')
-Router.post('/:post_id', CommentController.CreateComment)
-Router.delete('/:comment_id', CommentController.RemoveComment)
+Router.post('/:post_id/user/:user_id', CommentController.CreateComment)
+Router.delete('/:post_id/remove/:comment_id', CommentController.RemoveComment)
 Router.put('/:comment_id', CommentController.UpdateComment)
 
 module.exports = Router
